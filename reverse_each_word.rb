7062c757f8bd6bcfa34f59fe236a8369b717517b
Dean Erickson <dmepilot@yahoo.com>
2020-06-07 04:43:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   each_word =string.split(" ")
   reversed=[]
-  reversed=each_word.each do |sentence|
+  reversed=each_word.collect do |sentence|
     sentence.reverse!
   end
   reversed.join(" ")
